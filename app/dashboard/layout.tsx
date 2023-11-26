2855fc1a9b79dbe3ab208134242a892105b242d9
shouvikantu <sahmed@willamette.edu>
2023-11-26 00:05:51 -0800

    coommit

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,10 @@
 import SideNav from '../ui/dashboard/sidenav';
 
-export default function ({ children }: { children: React.ReactNode }) {
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
