bd08e9918d72f925bf053cf7fc1c38c30b1c16c7
shouvikantu <sahmed@willamette.edu>
2023-11-26 01:44:43 -0800

    Invoices pages working

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,30 @@
+import { fetchCardData, fetchLatestInvoices } from '../../lib/data';
+import CardWrapper, { Card } from '../../ui/dashboard/cards';
+import LatestInvoices from '../../ui/dashboard/latest-invoices';
+import RevenueChart from '../../ui/dashboard/revenue-chart';
+import { Suspense } from 'react';
+import {
+  CardSkeleton,
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from '@/app/ui/skeletons';
+export default async function Page() {
+  return (
+    <main>
+      <h1 className="mb-4 text-xl md:text-2xl">Dashboard</h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<CardSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
